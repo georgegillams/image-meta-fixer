e1a3844e34a5cb4133c4b24ba2d01fb580ea590f
George Gillams <code@georgegillams.co.uk>
2022-10-29 20:54:50 +0100

    fix: Add CLI arguments for fixing/moving

--- README.md
+++ README.md
@@ -5,12 +5,9 @@ Replaces file `created` and `content created` metadata with timestamp from exif.
 ## Usage — fix metadata and sort
 
 `cd` to this directory.
-Run `yarn action <INPUT_DIRECTORY> <OUTPUT_DIRECTORY>`
-
+Run `yarn action <INPUT_DIRECTORY> <OUTPUT_DIRECTORY> --fix --move`
 
 ## Usage — set metadata and sort
 
 `cd` to this directory.
-Run `yarn set <INPUT_FILE> <DATE> <OUTPUT_DIRECTORY>` where `DATE` is in format `YYYY:MM:DD HH:mm:ss`
-
-
+Run `yarn set <INPUT_FILE(S)> <DATE> <OUTPUT_DIRECTORY>` where `<DATE>` is in format `YYYY:MM:DD HH:mm:ss` and `<INPUT_FILE(S)>` is either a single file path or a directory.
--- src/fix-data.js
+++ src/fix-data.js
@@ -21,6 +21,8 @@ if (!outputDirectoryArg) {
   console.error("No output directory specified");
   process.exit(1);
 }
+const shouldFix = process.argv.includes("--fix");
+const shouldMove = process.argv.includes("--move");
 
 const inputDirectory = path.resolve(inputDirectoryArg);
 const outputDirectory = path.resolve(outputDirectoryArg);
@@ -32,65 +34,73 @@ const imagesToFix = filterFilesByAcceptedTypes(
 );
 console.log(`images`, imagesToFix);
 console.log(`${imagesToFix.length} files found`);
+if (shouldFix) {
+  console.log(`Will now fix dates in those files`);
+}
+if (shouldMove) {
+  console.log(`Will now move those files according to their dates`);
+}
 
 for (let image of imagesToFix) {
   const fullFilePath = path.join(inputDirectory, image);
   let actualCreateDate = null;
-  let hasErrored = false;
 
-  try {
-    const fileCreateDate = getExifDate(fullFilePath, "FileCreateDate");
-    const fileModifyDate = getExifDate(fullFilePath, "FileModifyDate");
-    actualCreateDate =
-      getExifDate(fullFilePath, "CreateDate") || getFileNameDate(fullFilePath);
+  if (shouldFix) {
+    try {
+      const fileCreateDate = getExifDate(fullFilePath, "FileCreateDate");
+      const fileModifyDate = getExifDate(fullFilePath, "FileModifyDate");
+      actualCreateDate =
+        getExifDate(fullFilePath, "CreateDate") ||
+        getFileNameDate(fullFilePath);
 
-    let canFix = true;
+      let canFix = true;
 
-    if (!actualCreateDate) {
-      console.log(`${image}: no real creation date`);
-      canFix = false;
-    }
-
-    if (
-      `${fileCreateDate} ${fileModifyDate}` ===
-      `${actualCreateDate} ${actualCreateDate}`
-    ) {
-      console.log(`${image}: dates match`);
-      canFix = false;
-    }
-
-    if (canFix) {
-      console.log(`${image}: fixing date to ${actualCreateDate}`);
-      execSync(
-        `exiftool "-FileCreateDate<CreateDate" "-FileModifyDate<CreateDate" "${fullFilePath}"`
-      );
-    }
-  } catch (e) {
-    hasErrored = true;
-    console.log(`${image}: error editing metadata`, e);
-  }
-
-  try {
-    let moveDirectory = path.join(outputDirectory, "unknown-date");
-    if (actualCreateDate) {
-      const [year, month] = actualCreateDate.split("-");
-      moveDirectory = path.join(outputDirectory, year, month);
-    }
-    let moveFileLocation = path.join(moveDirectory, image.split("/").pop());
-    if (fullFilePath !== moveFileLocation) {
-      // Ensure destination file has unique name so
-      // we don't overwrite anything
-      while (fs.existsSync(moveFileLocation)) {
-        const ext = path.extname(moveFileLocation);
-        const base = path.basename(moveFileLocation, ext);
-        moveFileLocation = path.join(moveDirectory, `${base}-1${ext}`);
+      if (!actualCreateDate) {
+        console.log(`${image}: no real creation date`);
+        canFix = false;
       }
 
-      execSync(`mkdir -p "${moveDirectory}"`);
-      execSync(`mv "${fullFilePath}" "${moveFileLocation}"`);
+      if (
+        `${fileCreateDate} ${fileModifyDate}` ===
+        `${actualCreateDate} ${actualCreateDate}`
+      ) {
+        console.log(`${image}: dates match`);
+        canFix = false;
+      }
+
+      if (canFix) {
+        console.log(`${image}: fixing date to ${actualCreateDate}`);
+        execSync(
+          `exiftool "-FileCreateDate<CreateDate" "-FileModifyDate<CreateDate" "${fullFilePath}"`
+        );
+      }
+    } catch (e) {
+      console.log(`${image}: error editing metadata`, e);
     }
-  } catch (e) {
-    hasErrored = true;
-    console.log(`${image}: error moving image`, e);
+  }
+
+  if (shouldMove) {
+    try {
+      let moveDirectory = path.join(outputDirectory, "unknown-date");
+      if (actualCreateDate) {
+        const [year, month] = actualCreateDate.split("-");
+        moveDirectory = path.join(outputDirectory, year, month);
+      }
+      let moveFileLocation = path.join(moveDirectory, image.split("/").pop());
+      if (fullFilePath !== moveFileLocation) {
+        // Ensure destination file has unique name so
+        // we don't overwrite anything
+        while (fs.existsSync(moveFileLocation)) {
+          const ext = path.extname(moveFileLocation);
+          const base = path.basename(moveFileLocation, ext);
+          moveFileLocation = path.join(moveDirectory, `${base}-1${ext}`);
+        }
+
+        execSync(`mkdir -p "${moveDirectory}"`);
+        execSync(`mv "${fullFilePath}" "${moveFileLocation}"`);
+      }
+    } catch (e) {
+      console.log(`${image}: error moving image`, e);
+    }
   }
 }
